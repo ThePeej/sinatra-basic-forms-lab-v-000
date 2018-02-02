a3d68c0bb370f6249911e6b7cb68c9272433c3dd
PJ Frias <peejfrias@gmail.com>
2018-02-02 16:43:05 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,10 +11,7 @@ class App < Sinatra::Base
   end
   
   post '/new' do
-    @puppy = Puppy.new
-    @puppy.name = params[:name]
-    @puppy.breed = params[:breed]
-    @puppy.age = params[:age]
+    @puppy = Puppy.new(params[:name], params[:breed], params[:age])
     erb :display_puppy
   end
 
